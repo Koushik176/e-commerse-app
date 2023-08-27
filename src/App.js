2024034c6b3375de77e7f1a671ee7c4b9a5dd643
Koushik <koushikashok176@gmail.com>
2023-08-27 15:29:28 +0530

    AboutPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import { Route } from "react-router-dom/cjs/react-router-dom.min";
 import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <Switch>
         <Route path="/" exact>
           <HomePage />
+        </Route>
+        <Route path="/about">
+          <AboutPage />
         </Route>
       </Switch>
     </Layout>
---
+++ src/Pages/AboutPage.js
@@ -0,0 +1,7 @@
+import AboutPageContent from "../Components/About/AboutPageContent";
+
+const AboutPage = () => {
+  return <AboutPageContent />;
+};
+
+export default AboutPage;
