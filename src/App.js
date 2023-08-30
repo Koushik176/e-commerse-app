2ef97ac621428b22499191c83cc1b2ca235ffede
Koushik <koushikashok176@gmail.com>
2023-08-30 17:45:20 +0530

    Redirect to auth if not logged in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Switch } from "react-router-dom";
 
 import Layout from "./Components/Layout/Layout";
-import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import { Redirect, Route } from "react-router-dom/cjs/react-router-dom.min";
 import HomePage from "./Pages/HomePage";
 import AboutPage from "./Pages/AboutPage";
 import StorePage from "./Pages/StorePage";
@@ -10,8 +10,10 @@ import CartShowProvider from "./Context-store/Cart-Show/CartShowProvider";
 import CartProvider from "./Context-store/CartProvider";
 import ContactUsPage from "./Pages/ContactUsPage";
 import AuthPage from "./Pages/AuthPage";
+import AuthContext from "./Context-store/Auth-Context/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <CartShowProvider>
       <CartProvider>
@@ -21,7 +23,8 @@ function App() {
               <HomePage />
             </Route>
             <Route path="/products">
-              <StorePage />
+              {authCtx.isLoggedIn && <StorePage />}
+              {!authCtx.isLoggedIn && <Redirect to="/auth" />}
             </Route>
             <Route path="/about">
               <AboutPage />
